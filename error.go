6e85d6e8631d2f82810100ca73c7a1c52f0483f5
teawithsand <teawithsand@gmail.com>
2022-02-15 19:05:38 +0000

    Implemented additional errors for stream encryption

--- error.go
+++ error.go
@@ -17,3 +17,6 @@ var ErrInvalidNonceType = errors.New("crypka: invalid NonceType value was provid
 var ErrInvalidRNGSeed = errors.New("crypka: given RNG seed is not valid")
 var ErrRNGOutOfEntropy = errors.New("crypka: given RNG ran out of entropy and can't generate random data anymore")
 var ErrInvalidKXDestination = errors.New("crypka: given KX destination buffer is not valid")
+
+var ErrStreamChunkTooBig = errors.New("crypka: streamming encryption chunk is too big and won't be decrypted")
+var ErrStreamCorrupted = errors.New("crypka: stream chunks were corrupted or reordered or stream was truncated or finalization chunks was not found")
